f8783961e63e9eeb736ef41047f84e3dad823e2a
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-11 10:03:56 -0700

    finished pt1

---
+++ process.py
@@ -0,0 +1,21 @@
+
+
+
+#opening the .txt. file and setting it to a variable
+log_file = open("um-server-01.txt")
+
+#function
+def sales_reports(log_file):
+    #for loop
+    for line in log_file:
+        #removing any trailing characters at the end of a string
+        line = line.rstrip()
+        #setting an item in the list to a variable to call it
+        day = line[0:3]
+        #if function to print the day variable if it is equal to the value
+        if day == "Mon":
+            #console.log
+            print(line)
+
+#inciting the function
+sales_reports(log_file)
